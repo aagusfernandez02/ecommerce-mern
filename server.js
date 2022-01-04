a7770c709e448237a99ee882d246670e469a949e
Agustin Fernandez <af.agusfernandez02@gmail.com>
2022-01-04 11:20:58 -0300

    setup

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileUpload({
+    useTempFiles: true
+}));
+
+//Connection to mongoDB
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI), err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+};
+
+app.get('/', (req, res) => {
+    res.json({ msg: "Hello world" })
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
