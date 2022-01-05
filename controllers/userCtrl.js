5c8d628577861f10350c7cb9d5b85eb8eaa063c2
Agustin Fernandez <af.agusfernandez02@gmail.com>
2022-01-05 10:31:09 -0300

    login - logout - getinfo for user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -32,9 +32,40 @@ const userCtrl = {
                 path: '/user/refresh_token',
             })
 
-            console.log(res.cookies)
+            res.json({ accesstoken });
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+    login: async(req, res) => {
+        try {
+            const { email, password } = req.body;
+
+            const user = await Users.findOne({ email });
+            if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({ msg: "Incorrect password." });
+
+            // If login success, create access token and refresh token
+            const accesstoken = createAccessToken({ id: user._id });
+            const refreshtoken = createRefreshToken({ id: user._id });
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+            })
 
             res.json({ accesstoken });
+
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+    logout: async(req, res) => {
+        try {
+            res.clearCookie('refreshtoken', { path: '/user/refresh_token' });
+            return res.json({ msg: "Logged out." });
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
@@ -56,6 +87,16 @@ const userCtrl = {
             return res.status(500).json({ msg: error.message })
         }
     },
+    getUser: async(req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+            if (!user) return res.status(500).json({ msg: "User does not exist" });
+
+            res.json(user);
+        } catch (error) {
+            return res.status(500).json({ msg: error.message });
+        }
+    }
 };
 
 const createAccessToken = (user) => {
